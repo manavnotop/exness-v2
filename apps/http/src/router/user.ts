ecd6176c2f9d671707a17f82b2b8a905dd0246b4
manavnotop <manuagarwal1443@gmail.com>
2025-09-05 23:10:14 +0530

    http: add rate limiter to user login

--- apps/http/src/router/user.ts
+++ apps/http/src/router/user.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { signinController, signupController } from "../controller/authController";
+import { rateLimit } from 'express-rate-limit';
 
 const userRouter: Router = Router();
 
-userRouter.route("/signup").post(signupController);
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000,
+	limit: 5, 
+	standardHeaders: 'draft-8',
+	legacyHeaders: false, 
+	ipv6Subnet: 56, 
+})
+
+userRouter.route("/signup").post(limiter, signupController);
 userRouter.route("/signin/post").get(signinController);
 
 export default userRouter;
